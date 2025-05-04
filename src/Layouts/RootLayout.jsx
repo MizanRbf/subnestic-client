717876d84a053bf667e8450f401b4ef9faf286f8
MizanRbf <mizanrbf@gmail.com>
2025-05-04 23:52:00 +0600

    navbar & footer added but not customized , later will be customized

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { Outlet } from "react-router";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
 const RootLayout = () => {
   return (
     <div>
-      <header>This is Header</header>
-      <main>
+      <header className="w-11/12 mx-auto">
+        {/* Navbar */}
+        <Navbar></Navbar>
+      </header>
+
+      {/* Main */}
+      <main className="w-11/12 mx-auto">
         <Outlet></Outlet>
       </main>
-      <footer>This is footer</footer>
+
+      {/* Footer */}
+      <footer className="text-center">
+        <Footer></Footer>
+      </footer>
     </div>
   );
 };
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,18 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../Layouts/RootLayout";
 import ErrorPage from "../Pages/ErrorPage";
+import Home from "../Pages/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout></RootLayout>,
+    children: [
+      {
+        index: true,
+        element: <Home></Home>,
+      },
+    ],
   },
   {
     path: "/*",
