87c0d7f274d2fcc661260e2d5bd1ae5cdb681082
MizanRbf <mizanrbf@gmail.com>
2025-05-04 22:56:46 +0600

    subscription box projectc initialized

---
+++ src/Layouts/RootLayout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const RootLayout = () => {
+  return (
+    <div>
+      <header>This is Header</header>
+      <main>This is Main Section</main>
+      <footer>This is footer</footer>
+    </div>
+  );
+};
+
+export default RootLayout;
