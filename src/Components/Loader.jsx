a63bcede1c298c623b3e193f9a5c62bb1d04e3d8
MizanRbf <mizanrbf@gmail.com>
2025-05-06 10:25:37 +0600

    Loader style updated

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <div>
-      <h1>
-        Loading <span className="loading loading-dots loading-xl"></span>
-      </h1>
+    <div className="flex justify-center items-center min-h-[calc(100vh-288px)]">
+      <h2>
+        Data Loading <span className="loading loading-dots loading-xl"></span>
+      </h2>
     </div>
   );
 };
